d76e235310c30c95dbaa87aa5490371aa3878efc
Suraj Pai <surajballambat@gmail.com>
2017-05-07 00:58:02 +0530

    created module and notebook

--- lemon.py
+++ lemon.py
@@ -9,6 +9,7 @@ class LemonRecognizer(object):
         self.image = cv2.imread(image_file)
         self.circles = None
         self.radius = None
+        self.feature_image = None
 
     def get_image(self):
         return self.image
@@ -19,7 +20,7 @@ class LemonRecognizer(object):
         :return: Minimum circle radius
         """
         hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        ## Extract Hue Value
+        ## Extract Hue Value from HSV representation ( Taking lemon hue as a feature extractor )
         im = hsv[:, :, 0]
         ## Blur image to remove noise
         im = cv2.blur(im, (3, 3))
@@ -28,42 +29,24 @@ class LemonRecognizer(object):
         ret, bw = cv2.threshold(im, 0 , 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         bw = cv2.blur(bw, (4, 4))
 
-        ## Start finding contour
+        ## Start finding contour using opencv contour library
         contour, hierarchy = cv2.findContours(bw, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE)
         cnt = contour[0]
         (x, y), self.radius = cv2.minEnclosingCircle(cnt)
+        self.feature_image = bw
         return (x, y), self.radius
 
     def hough_circle_transform(self):
+        """
+        Use hough circle method to find circles in the image to fit a given criteria ( In this case, 
+        to find circles close to the minimum enclosed circle from contour estimation
+        :return: 
+        """
         r_eff = int(self.radius)
-        circles = cv2.HoughCircles(bw, method=cv.CV_HOUGH_GRADIENT, dp=5, minDist=int(r_eff * 0.5 + 1),
+        circles = cv2.HoughCircles(self.feature_image, method=cv.CV_HOUGH_GRADIENT, dp=5, minDist=int(r_eff * 0.5 + 1),
                                    minRadius=int(r_eff - r_eff * 0.4), maxRadius=int(r_eff + r_eff * 0.009),
                                    param1=10, param2=int(2 * 3.14 * int(r_eff - r_eff * 0.57)))
         self.circles = circles[0, :]
         return self.circles
 
-# start = timeit.default_timer()
-# img = cv2.imread('/home/suraj/Repositories/LemonRecognition/lemon.jpeg')
-# hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-# im = hsv[:, :, 0]
-# im = cv2.blur(im,(3, 3))
-# im = cv2.bitwise_not(im)
-# ret, bw = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-# bw = cv2.blur(bw,(4, 4))
-# contour, hierarchy = cv2.findContours(bw, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE)
-# cnt = contour[0]
-# (x,y),radius = cv2.minEnclosingCircle(cnt)
-# r_eff = int(radius)
-# circles = cv2.HoughCircles(bw, method=cv.CV_HOUGH_GRADIENT, dp=5,  minDist= int(r_eff * 0.5 + 1),
-#                            minRadius= int(r_eff - r_eff * 0.4) , maxRadius= int(r_eff + r_eff * 0.009),
-#                            param1= 10, param2= int(2 * 3.14 * int( r_eff - r_eff * 0.57)))
-# circles = np.uint16(np.around(circles))
-#
-# for r in circles[0,:]:
-#     cv2.circle(img,(r[0],r[1]),r[2],(255,255,255),2)
-#
-# print len(circles[0])
-# print (timeit.default_timer() - start ) * 1000
-# cv2.imshow('detected circles',img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
